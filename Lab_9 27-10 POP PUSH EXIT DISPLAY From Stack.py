607b47bfb6b8cdffe3b53dcb9dcd0d308875eed0
Jithendrachowdary48 <70189640+Jithendrachowdary48@users.noreply.github.com>
2020-12-15 09:46:53 +0530

    Add files via upload

---
+++ Lab_9 27-10 POP PUSH EXIT DISPLAY From Stack.py
@@ -0,0 +1,12 @@
+#POP PUSH EXIT DISPLAY A SPECIFIC ELEMENT FROM STACK
+stack = ["NTR", "RAM", "RRR"] 
+print(stack)
+# PUSH ELEMENT
+stack.append("MS") 
+stack.append("SAM") 
+print(stack) 
+# POP ELEMENT
+print(stack.pop( )) 
+print(stack)
+exit
+ 
