6536cd993afc31523f097a1fdbb36630d154b99a
Jithendrachowdary48 <70189640+Jithendrachowdary48@users.noreply.github.com>
2020-12-15 09:54:42 +0530

    Add files via upload

---
+++ Lab_9 27-10 pop push exit display From Stack.py
@@ -0,0 +1,12 @@
+#program to implement pop push and exit
+stack = [10,20,30] 
+print(stack)
+#push
+stack.append(40) 
+stack.append(50) 
+print(stack) 
+#pop
+print(stack.pop( )) 
+print(stack)
+exit
+ 
